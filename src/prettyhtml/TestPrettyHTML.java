fe8c76baa8462ce1daa51c4dd1914789d0a94e5a
makv3r <makv3r@gmail.com>
2025-01-30 22:06:34 +0200

    TestPrettyHTML class changes

--- src/prettyhtml/TestPrettyHTML.java
+++ src/prettyhtml/TestPrettyHTML.java
@@ -5,14 +5,10 @@ import util.FileUtils;
 import java.io.IOException;
 
 public class TestPrettyHTML {
-    private final static String html = """
-            
-            """;
-
     public static void main(String[] args) {
         try {
-            FileUtils.writeFile("output1.txt", html);
-            FileUtils.writeFile("output2.txt", PrettyHTML.applyTidy(html));
+            String html = FileUtils.readFile("input.txt");
+            FileUtils.writeFile("output.txt", PrettyHTML.applyTidy(html));
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
